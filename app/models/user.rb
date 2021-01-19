147833509c2fb51682fef2daa913ccf12faad592
matsu006007 <76504047+matsu006007@users.noreply.github.com>
2021-01-19 18:47:57 +0900

    実装①

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+
+  validates :birthday, presence: true
+
+  PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze,/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/.freeze
+  validates :password, format: { with: PASSWORD_REGEX, message: 'は英数字混合にしてください' }
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/} do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/} do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+end
