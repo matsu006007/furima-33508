5b23af2517d2e5f5f99774453cf5b86c74d204bf
matsu006007 <76504047+matsu006007@users.noreply.github.com>
2021-01-25 14:29:20 +0900

    Rubocop　修正　１５

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,69 +13,69 @@ RSpec.describe Item, type: :model do
     end
     context '商品が出品できないとき' do
       it '商品画像が空では出品できない' do
-       @item.image = nil
-       @item.valid?
-       expect(@item.errors.full_messages).to include("Image can't be blank")
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end
       it '商品名が空では出品できない' do
-       @item.product_name = nil
-       @item.valid?
-       expect(@item.errors.full_messages).to include("Product name can't be blank")
+        @item.product_name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Product name can't be blank")
       end
       it '商品の説明が空では出品できない' do
-       @item.product_description = nil
-       @item.valid?
-       expect(@item.errors.full_messages).to include("Product description can't be blank")
+        @item.product_description = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Product description can't be blank")
       end
       it 'カテゴリーの情報のidが１では出品できない' do
-       @item.category_id = 1
-       @item.valid?
-       expect(@item.errors.full_messages).to include("Category must be other than 1")
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Category must be other than 1')
       end
       it '商品の状態についての情報のidが１では出品できない' do
-       @item.product_status_id = 1
-       @item.valid?
-       expect(@item.errors.full_messages).to include("Product status must be other than 1")
+        @item.product_status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Product status must be other than 1')
       end
       it '配送料の負担についての情報のidが１では出品できない' do
-       @item.freight_id = 1
-       @item.valid?
-       expect(@item.errors.full_messages).to include("Freight must be other than 1")
+        @item.freight_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Freight must be other than 1')
       end
       it '発送元の地域についての情報のidが１では出品できない' do
-       @item.ship_region_id = 1
-       @item.valid?
-       expect(@item.errors.full_messages).to include("Ship region must be other than 1")
+        @item.ship_region_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Ship region must be other than 1')
       end
       it '発送までの日数についての情報のidが１では出品できない' do
         @item.ship_date_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Ship date must be other than 1")
+        expect(@item.errors.full_messages).to include('Ship date must be other than 1')
       end
       it '価格についての情報が空では出品できない' do
         @item.price = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank", "Price is invalid", "Price Out of setting range")
+        expect(@item.errors.full_messages).to include("Price can't be blank", 'Price is invalid', 'Price Out of setting range')
       end
       it '価格が¥300以下では出品できない' do
         @item.price = 100
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price Out of setting range")
+        expect(@item.errors.full_messages).to include('Price Out of setting range')
       end
       it '価格が¥9,999,999以上では出品できない' do
-        @item.price = 10000000
+        @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price Out of setting range")
+        expect(@item.errors.full_messages).to include('Price Out of setting range')
       end
       it '価格が全角数字では出品できない' do
-        @item.price = "５５５５５"
+        @item.price = '５５５５５'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price Out of setting range")
+        expect(@item.errors.full_messages).to include('Price Out of setting range')
       end
       it 'ユーザーが紐付いていないと登録できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("User must exist")
+        expect(@item.errors.full_messages).to include('User must exist')
       end
     end
   end
