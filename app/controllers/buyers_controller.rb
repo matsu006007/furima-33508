7205ec779088e117f116c005eeabf3c1f326afd6
matsu006007 <76504047+matsu006007@users.noreply.github.com>
2021-02-03 09:31:35 +0900

    Rubocopによる修正１

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,10 +1,9 @@
 class BuyersController < ApplicationController
   before_action :authenticate_user!
   before_action :item_data
-  
 
   def index
-    if @item.user == current_user
+    if @item.user == current_user || @item.buyer.present?
       redirect_to root_path
     else
       @buy_info = BuyInfo.new
@@ -13,19 +12,21 @@ class BuyersController < ApplicationController
 
   def create
     @buy_info = BuyInfo.new(info_params)
-     if @buy_info.valid?
+    if @buy_info.valid?
       credit_item
       @buy_info.save
       redirect_to root_path
-     else
-      render  :index
-     end
+    else
+      render :index
+    end
   end
 
   private
 
   def info_params
-    params.require(:buy_info).permit(:post_code, :ship_region_id, :city, :block, :building, :phone).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+    params.require(:buy_info).permit(:post_code, :ship_region_id, :city, :block, :building, :phone).merge(
+      user_id: current_user.id, item_id: @item.id, token: params[:token]
+    )
   end
 
   def item_data
@@ -33,12 +34,11 @@ class BuyersController < ApplicationController
   end
 
   def credit_item
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      Payjp::Charge.create(
-        amount: @item.price,
-        card: info_params[:token],
-        currency: 'jpy'
-      )
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: info_params[:token],
+      currency: 'jpy'
+    )
   end
-
 end
