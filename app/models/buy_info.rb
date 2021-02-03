6791f8ff346f6ecadf77c60be3a3a6e3bf5c5213
matsu006007 <76504047+matsu006007@users.noreply.github.com>
2021-02-03 09:31:51 +0900

    Rubocopによる修正３

--- app/models/buy_info.rb
+++ app/models/buy_info.rb
@@ -1,11 +1,12 @@
 class BuyInfo
   include ActiveModel::Model
   attr_accessor :post_code, :ship_region_id, :city, :block, :building, :phone, :user_id, :item_id, :token
+
   with_options presence: true do
-    validates :post_code,  format: {with: /\A[0-9\d]{3}-[0-9\d]{4}\z/}
+    validates :post_code, format: { with: /\A[0-9\d]{3}-[0-9\d]{4}\z/ }
     validates :city
     validates :block
-    validates :phone, format: {with: /\A[0-9]+\z/}, length: { maximum: 11 }
+    validates :phone, format: { with: /\A[0-9]+\z/ }, length: { maximum: 11 }
     validates :ship_region_id, numericality: { other_than: 1 }
     validates :user_id
     validates :item_id
@@ -13,7 +14,8 @@ class BuyInfo
   end
 
   def save
-   buyer = Buyer.create(user_id: user_id, item_id: item_id)
-   Payment.create(post_code: post_code, ship_region_id: ship_region_id, city: city, block: block, building: building, phone: phone, buyer_id: buyer.id)
+    buyer = Buyer.create(user_id: user_id, item_id: item_id)
+    Payment.create(post_code: post_code, ship_region_id: ship_region_id, city: city, block: block, building: building,
+                   phone: phone, buyer_id: buyer.id)
   end
 end
